ad354664382aa97936db769421c8a4bbe8a9df3c
Colin O'Flynn <coflynn@newae.com>
2025-03-07 15:24:07 -0400

    Add interface board for IR on safe

---
+++ challenge_safe/safe_ir_sketch/safe_ir_sketch.ino
@@ -0,0 +1,87 @@
+#include <SoftwareSerial.h>
+#include <stdlib.h>
+
+const byte rxPinIgnored = 10;
+const byte txPinforLED = 7;
+
+// Set up a new SoftwareSerial object
+SoftwareSerial breakSerial (rxPinIgnored, txPinforLED);
+
+/****************************
+
+ IR Blaster for Safe
+
+ Connect IR led as follows, cathode to Pin 7,
+ anode to Pin 11:
+
+            1K       C   A
+ Pin 7 --/\/\/\/\-----|<|-----------+
+                                    |
+ Pin 11 ----------------------------+
+
+ Connect safe serial output to RX input.
+ 
+ ****************************/
+
+void setup() {
+    pinMode(11, OUTPUT); // Using Pin 11 for Timer2 on Arduino Uno
+    
+    TCCR2A = 0; // Clear Timer2 Control Register A
+    TCCR2A |= (1 << WGM21); // Set to CTC Mode
+    TCCR2A |= (1 << COM2A0); // Toggle OC2A on Compare Match
+    
+    TCCR2B = 0; // Clear Timer2 Control Register B
+    TCCR2B |= (1 << CS20); // Set prescaler to 1 (No prescaling)
+    
+    // Calculate OCR2A for approximately 59 kHz
+    OCR2A = (16000000 / (2 * 1 * 59000)) - 1;
+
+    pinMode(rxPinIgnored, INPUT_PULLUP);
+    pinMode(txPinforLED, OUTPUT);
+
+    breakSerial.begin(600);
+    Serial.begin(9600);   
+}
+
+uint8_t chksum(char * data, unsigned char len){
+  uint8_t sum = 0;
+
+  while(len){
+    sum ^= *data++;
+    len--;
+  }
+  
+  return sum; 
+}
+
+void serialFlush(){
+  while(Serial.available() > 0) {
+    char t = Serial.read();
+  }
+}
+
+char tempsafe[20];
+char tempbeacon[20];
+int lenRead = 0;
+char c;
+
+void loop() {
+  if (Serial.available()) {
+    c = Serial.read();
+    tempsafe[lenRead++] = c;
+  }
+
+  if(lenRead > 3){
+    lenRead = 0;
+  }
+
+  if ((c == "L") || (c == "U") && (lenRead == 2)){
+    sprintf(tempbeacon, "SAF %c:%c", tempsafe[0], tempsafe[1]);
+    lenRead = 0;
+
+    breakSerial.write(tempbeacon);
+    sprintf(tempbeacon, " %02x\n", chksum(tempbeacon, 7));
+    breakSerial.write(tempbeacon); 
+  }
+
+}
